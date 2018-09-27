1352e3ce09d40622e4afde449799ee5d0302bd5c
ruvdveld <ruben.vandevelde@student.hogent.be>
2018-09-27 12:14:25 +0200

    Creëer de klasse BankAccount

---
+++ Banking2C1/Models/Domain/BankAccount.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking2C1.Models.Domain
+{
+    public class BankAccount
+    {
+        #region comments
+        /*private string _accountNumber;
+        private decimal _balance;
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        private void SetBalance(decimal value)
+        {
+            _balance = value;
+        }*/
+
+        //public decimal Balance { get; private set; } //identiek als al het bovenstaande ivm balance! => automatic property
+        /*
+         gebruiken? vb bankaccount ba:
+         ba.Balance = 20;
+         console.writeLine(ba.Balance); 
+         ba.Balance = ba.Balance + 100;
+         */ 
+        #endregion
+
+        #region fields
+        public const decimal WithdrawCost = 0.10M;
+        private decimal _balance;
+        #endregion
+
+        #region properties
+        public string AccountNumber { get; set; }
+        public decimal Balance
+        {
+            get { return _balance; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Invalid value for balance"); //voorwaarde toevoegen
+                _balance = value; //waarde van property is binnen property gekend als "value"
+            }
+        }
+        #endregion
+
+        #region constructors
+        public BankAccount(String accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public BankAccount(String accountNumber, decimal bedrag) : this(accountNumber)
+        {
+            //AccountNumber = accountNumber; of: zie hoofding constructor
+            Balance = bedrag;
+        }
+        #endregion
+
+        #region methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount + WithdrawCost;
+        } 
+        #endregion
+    }
+}
--- Banking2C1/Program.cs
+++ Banking2C1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking2C1.Models.Domain;
+using System;
 
 namespace Banking2C1
 {
@@ -6,7 +7,26 @@ namespace Banking2C1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount myBa = new BankAccount("123-123123123-23")
+            {
+                Balance = 50
+            };
+            BankAccount anotherBa = new BankAccount("456-456456-456-99", 150658);
+
+            Console.WriteLine($"Balance is currently {anotherBa.Balance} euro");
+
+            anotherBa.Deposit(10000);
+
+            Console.WriteLine($"Your salary has been paid");
+            Console.WriteLine($"Your salary has been paid");
+            Console.WriteLine($"Balance is currently {anotherBa.Balance} euro");
+
+            anotherBa.Withdraw(1259);
+
+            Console.WriteLine("You've bought the new iPhone xs max!");
+            Console.WriteLine($"Balance is currently {anotherBa.Balance} euro");
+
+            Console.ReadLine();
         }
     }
 }
